a5afd8009ef124e67bf078bc3b2a5c869a97352f
Lukas Roth <mail@lukasroth.net>
2023-04-18 15:22:20 +0200

    motor-nx-media to syntactic sugar

--- pages/admin/motor-media/files/create.vue
+++ pages/admin/motor-media/files/create.vue
@@ -11,13 +11,13 @@
       <div class="col-md-8">
         <div class="row">
           <div class="col-md-6">
-            {{model}}
+            {{ model }}
             <FormsInputField
               type="text"
               name="description"
               id="description"
               :label="$t('motor-media.files.description')"
-              :value="model.description"
+              v-model="model.description"
             ></FormsInputField>
           </div>
           <div class="col-md-6">
@@ -26,7 +26,7 @@
               name="author"
               id="author"
               :label="$t('motor-media.files.author')"
-              :value="model.author"
+              v-model="model.author"
             ></FormsInputField>
           </div>
           <div class="col-md-6">
@@ -35,7 +35,7 @@
               name="source"
               id="source"
               :label="$t('motor-media.files.source')"
-              :value="model.source"
+              v-model="model.source"
             ></FormsInputField>
           </div>
           <div class="col-md-6">
@@ -44,18 +44,18 @@
               name="alt_text"
               id="alt_text"
               :label="$t('motor-media.files.alt_text')"
-              :value="model.alt_text"
+              v-model="model.alt_text"
             ></FormsInputField>
           </div>
           <div class="col-md-12">
-            <FileUploadField
+            <FormsFileUploadField
               name="files"
               id="files"
               :allow-delete="true"
               :multiple="true"
               :label="$t('motor-media.files.file')"
-              :value="model.files"
-            ></FileUploadField>
+              v-model="model.files"
+            ></FormsFileUploadField>
           </div>
         </div>
       </div>
@@ -64,63 +64,39 @@
           name="categories"
           id="categories"
           :label="$t('motor-admin.categories.categories')"
-          :value="model.categories"
+          v-model="model.categories"
           :tree="treeData.children"
         ></FormsCheckboxTreeField>
       </div>
     </div>
   </AdminCommonForm>
 </template>
-<script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
-import AdminCommonForm from '@zrm/motor-nx-core/components/admin/common/Form.vue'
-import FormsInputField from '@zrm/motor-nx-core/components/forms/InputField.vue'
-import FileUploadField from '@zrm/motor-nx-core/components/forms/FileUploadField.vue'
-import FormsCheckboxTreeField from '@zrm/motor-nx-core/components/forms/CheckboxTreeField.vue'
-import { useI18n } from 'vue-i18n'
+<script setup lang="ts">
+import {useI18n} from 'vue-i18n'
 import form from '@zrm/motor-nx-media/forms/fileForm'
 
-export default defineComponent({
-  name: 'admin-motor-media-files-create',
-  components: {
-    AdminCommonForm,
-    FormsInputField,
-    FormsCheckboxTreeField,
-    FileUploadField,
-  },
-  setup() {
-    // Load i18n module
-    const { t } = useI18n()
+// Load i18n module
+const {t} = useI18n()
 
-    // Load form
-    const { model, onSubmit, treeData } = form()
+// Load form
+const {model, onSubmit, treeData} = form()
 
-    // Set default action title
-    const title = ref(t('motor-media.files.new'))
+// Set default action title
+const title = ref(t('motor-media.files.new'))
 
-    const multiple = ref(false)
+const multiple = ref(false)
 
 
-    // Sanitize roles
-    watch(model, () => {
-      const checkAgainst = Object.entries(model.value.categories)
-      const options = []
-      for (const object of checkAgainst) {
-        const checkObject: any = object
-        if (checkObject[1]) {
-          options.push(checkObject[1]['id'])
-        }
-      }
-      model.value.categories = options
-    })
-
-    return {
-      model,
-      title,
-      onSubmit,
-      treeData,
-      multiple,
+// Sanitize roles
+watch(model, () => {
+  const checkAgainst = Object.entries(model.value.categories)
+  const options = []
+  for (const object of checkAgainst) {
+    const checkObject: any = object
+    if (checkObject[1]) {
+      options.push(checkObject[1]['id'])
     }
-  },
+  }
+  model.value.categories = options
 })
 </script>
--- pages/admin/motor-media/files/index.vue
+++ pages/admin/motor-media/files/index.vue
@@ -14,107 +14,82 @@
   ></AdminCommonGrid>
 </template>
 
-<script lang="ts">
-import AdminCommonGrid from '@zrm/motor-nx-core/components/admin/common/Grid.vue'
-import { defineComponent, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-import EditButton from '@zrm/motor-nx-core/components/admin/cell/EditButton.vue'
-import DeleteButton from '@zrm/motor-nx-core/components/admin/cell/DeleteButton.vue'
+<script setup lang="ts">
+import {useI18n} from 'vue-i18n'
 import grid from '@zrm/motor-nx-media/grids/fileGrid'
 import CellFile from '@zrm/motor-nx-core/components/admin/cell/File.vue'
 import categoryRepository from '@zrm/motor-nx-admin/api/category'
 
-export default defineComponent({
-  name: 'admin-motor-media.files',
-  components: {
-    AdminCommonGrid,
+// Load i18n module
+const {t} = useI18n()
+
+// Define columns for grid
+const columns = ref([
+  {
+    name: t('motor-media.files.file'),
+    prop: 'file',
+    components: [{name: 'CellFile'}],
   },
-  setup() {
-    // Load i18n module
-    const { t } = useI18n()
-
-    // Define columns for grid
-    const columns = ref([
+  {
+    name: t('motor-media.files.name'),
+    prop: 'file.file_name',
+  },
+  {
+    name: t('motor-media.files.mime_type'),
+    prop: 'file.mime_type',
+  },
+  {
+    name: '',
+    prop: 'actions',
+    columnStyle: 'width: 200px',
+    rowWrapperClass: 'justify-content-end',
+    components: [
       {
-        name: t('motor-media.files.file'),
-        prop: 'file',
-        components: [{ name: 'CellFile' }],
-      },
-      {
-        name: t('motor-media.files.name'),
-        prop: 'file.file_name',
-      },
-      {
-        name: t('motor-media.files.mime_type'),
-        prop: 'file.mime_type',
-      },
-      {
-        name: '',
-        prop: 'actions',
-        columnStyle: 'width: 200px',
-        rowWrapperClass: 'justify-content-end',
-        components: [
-          {
-            name: 'EditButton',
-            options: {
-              route: 'admin.motor-media.files.edit',
-              name: t('global.edit'),
-            },
-          },
-          { name: 'DeleteButton', options: { name: t('motor-admin.global.delete') } },
-        ],
-      },
-    ])
-
-    // Define filters for grid
-    const filters = ref([
-      { name: 'SearchFilter', options: {} },
-      {
-        name: 'SelectFilter',
+        name: 'EditButton',
         options: {
-          parameter: 'category_id',
-          emptyOption:
-            t('global.filter') + ': ' + t('motor-admin.categories.category'),
-          options: <any>[],
+          route: 'admin.motor-media.files.edit',
+          name: t('global.edit'),
         },
       },
-    ])
-
-    onMounted(async () => {
-      // Get catgories from api
-      const categoryRepo = categoryRepository()
-      const {data: response } = await categoryRepo.index({}, '1');
-      for (let i = 0; i < response.value.data.length; i++) {
-        filters.value[1].options.options.push({
-          name: response.value.data[i].name,
-          value: response.value.data[i].id,
-        })
-      }
-    })
-
-    const loadComponents = [
-      {
-        name: 'CellFile',
-        object: CellFile,
-      },
-    ]
-
-    // WE START THE OUTSOURCED CODE HERE
-    const { rows, meta, refreshRecords, handleCellEvent } = grid()
-
-    onMounted(async () => {
-      await refreshRecords();
-    })
-
-    return {
-      columns,
-      filters,
-      rows,
-      meta,
-      refreshRecords,
-      loadComponents,
-      handleCellEvent,
-    }
+      {name: 'DeleteButton', options: {name: t('motor-admin.global.delete')}},
+    ],
   },
+])
+
+// Define filters for grid
+const filters = ref([
+  {name: 'SearchFilter', options: {}},
+  {
+    name: 'SelectFilter',
+    options: {
+      parameter: 'category_id',
+      emptyOption:
+        t('global.filter') + ': ' + t('motor-admin.categories.category'),
+      options: <any>[],
+    },
+  },
+])
+
+const loadComponents = [
+  {
+    name: 'CellFile',
+    object: CellFile,
+  },
+]
+
+// WE START THE OUTSOURCED CODE HERE
+const {rows, meta, refreshRecords, handleCellEvent} = grid()
+
+onMounted(async () => {
+  await refreshRecords();
+  // Get catgories from api
+  const categoryRepo = categoryRepository()
+  const {data: response} = await categoryRepo.index({}, '1');
+  for (let i = 0; i < response.value.data.length; i++) {
+    filters.value[1].options.options.push({
+      name: response.value.data[i].name,
+      value: response.value.data[i].id,
+    })
+  }
 })
 </script>
