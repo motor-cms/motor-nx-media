cac71076a25a4ae963c61dd15c43683996582c7d
Lukas Roth <mail@lukasroth.net>
2023-09-08 10:58:17 +0200

    index ssr ready

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    imports: {
+        dirs: [
+            // Scan top-level modules
+            'composables',
+            // ... or scan modules nested one level deep with a specific name and file extension
+            'composables/*/index.{ts,js,mjs,mts}',
+            // ... or scan all modules within given directory
+            'composables/**',
+            'stores/**',
+            'stores/**'
+        ]
+    }
 })
